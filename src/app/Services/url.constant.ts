d0e24e0c4a440ed10739663c8efd165781e94535
Subhra Sankha Saha <jeesan2006@gmail.com>
2020-04-12 21:42:21 +0530

    update the api url

--- src/app/Services/url.constant.ts
+++ src/app/Services/url.constant.ts
@@ -1,2 +1,3 @@
-export const IisUrl = 'https://leavereqapp.com/api/';
+// export const IisUrl = 'https://leavereqapp.com/api/';
+export const IisUrl = 'https://ssswebappdemo.azurewebsites.net/api/';
 export const SolutionUrl = 'https://localhost:44364/api/';
